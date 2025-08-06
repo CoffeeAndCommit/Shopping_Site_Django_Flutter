be7130645eed2af849621b1b115e485ae122cddb
flutterexcel <deepak.excel2011@gmail.com>
2025-08-06 18:30:19 +0530

    tab notifire"

--- fashion_app/lib/src/home/views/home_screen.dart
+++ fashion_app/lib/src/home/views/home_screen.dart
@@ -1,13 +1,53 @@
+import 'package:fashion_app/common/utils/enums.dart';
+import 'package:fashion_app/src/home/controller/home_tab_notifier.dart';
 import 'package:fashion_app/src/home/widgets/categories_list.dart';
 import 'package:fashion_app/src/home/widgets/home_header.dart';
 import 'package:fashion_app/src/home/widgets/home_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/custom_appbar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController tabController;
+  int _currentTabIndex = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    tabController = TabController(length: homeTabs.length, vsync: this);
+    tabController.addListener(_handleSelection);
+
+    super.initState();
+  }
+
+  void _handleSelection() {
+    final controller = Provider.of<HomeTabNotifier>(context, listen: false);
+
+    if (tabController.indexIsChanging) {
+      setState(() {
+        _currentTabIndex = tabController.index;
+      });
+      controller.setIndex(homeTabs[_currentTabIndex]);
+    }
+  }
+@override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    tabController.removeListener(_handleSelection);
+    tabController.dispose();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -34,3 +74,12 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+List<String> homeTabs = [
+  'All',
+  'Popular',
+  'Unisexes',
+  'Mens',
+  'Womens',
+  'Kids',
+];
