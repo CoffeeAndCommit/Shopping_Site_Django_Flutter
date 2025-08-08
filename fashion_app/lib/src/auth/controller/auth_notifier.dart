4c6d831f4f98dce43e4d380b5fef49ee74ffb43f
flutterexcel <deepak.excel2011@gmail.com>
2025-08-08 12:24:56 +0530

    auth model and reg model done

---
+++ fashion_app/lib/src/auth/controller/auth_notifier.dart
@@ -0,0 +1,17 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class AuthNotifier  extends ChangeNotifier{
+
+
+  void loginFunction(String data) async {
+
+  }
+
+  void registrationFunction(String data) async {
+
+  }
+  
+}
