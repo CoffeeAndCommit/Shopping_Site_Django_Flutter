03a4cba1c0073a0b9bdf1e9078ff18bd2dc764bb
flutterexcel <deepak.excel2011@gmail.com>
2025-08-12 15:32:08 +0530

    worked on category page

--- fashion_app/lib/src/categories/view/category_screen.dart
+++ fashion_app/lib/src/categories/view/category_screen.dart
@@ -3,25 +3,35 @@ import 'package:fashion_app/common/utils/kstrings.dart';
 import 'package:fashion_app/common/widgets/app_style.dart';
 import 'package:fashion_app/common/widgets/back_button.dart';
 import 'package:fashion_app/common/widgets/reusable_text.dart';
+import 'package:fashion_app/common/widgets/shimmers/list_shimmer.dart';
 import 'package:fashion_app/const/constants.dart';
 import 'package:fashion_app/src/categories/controller/category_notifier.dart';
+import 'package:fashion_app/src/categories/hook/results/fetch_categories.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
-class CategoriesPage extends StatefulWidget {
+class CategoriesPage extends HookWidget {
   const CategoriesPage({super.key});
 
   @override
-  State<CategoriesPage> createState() => _CategoriesPageState();
-}
-
-class _CategoriesPageState extends State<CategoriesPage> {
-  @override
   Widget build(BuildContext context) {
+    final results = fetchCategories();
+    final categories = results.categories;
+
+    final isLoading = results.isLoading;
+
+    final error = results.error;
+
+    if (isLoading) {
+      return const Scaffold(
+        body: ListShimmer(),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         leading: const AppBackButton(),
@@ -35,8 +45,10 @@ class _CategoriesPageState extends State<CategoriesPage> {
           final category = categories[index];
           return ListTile(
             onTap: () {
-              context.read<CategoryNotifier>().setCategory(category.title, category.id);
-              
+              context
+                  .read<CategoryNotifier>()
+                  .setCategory(category.title, category.id);
+
               context.push('/category');
             },
             leading: CircleAvatar(
